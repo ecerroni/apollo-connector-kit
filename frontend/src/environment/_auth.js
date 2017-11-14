284cd20f7d5547e8820edd8c5eac4d6b6bc8e398
Rico <e.cerroni@gmail.com>
2017-11-14 10:49:31 +0100

    move

--- frontend/src/environment/_auth.js
+++ frontend/src/environment/_auth.js
@@ -1,5 +1,10 @@
 import { JWT as JWT_COMMON, APP, AUTH } from '#/common';
 
+// You may change this one only to match your needs
+export const CLIENT_AUTH_REQUEST_TYPE = AUTH.STRATEGIES.CLIENT.HTTP_ONLY; // Or
+// AUTH.STRATEGIES.CLIENT.HTTP_ONLY
+
+
 // eslint-disable-next-line
 export const JWT = {
   ...JWT_COMMON,
@@ -12,10 +17,6 @@ export const JWT = {
     },
   },
 };
-
-// You may change this to match your needs
-export const CLIENT_AUTH_REQUEST_TYPE = AUTH.STRATEGIES.CLIENT.HTTP_ONLY; // Or
-// AUTH.STRATEGIES.CLIENT.HTTP_ONLY
 
 export const CLIENT_AUTHENTICATION_METHOD = { // Either both FALSE or one TRUE at any given time
   HTTP_ONLY: AUTH.STRATEGIES.HTTP_ONLY &&
