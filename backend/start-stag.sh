0055aead050c69563bf9174cd6d3685a2a96104b
Rico <e.cerroni@gmail.com>
2017-11-16 16:36:48 +0100

    bash start scripts

---
+++ backend/start-stag.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+############# change these settings ###################
+SECRET_TOKEN_NAME="${ENV_SECRET1_NAME_HERE}"
+SECRET_REFRESH_TOKEN_NAME="${ENV_SECRET2_NAME_HERE}"
+#######################################################
+
+# all secret stuff and needed ENVs goes here before starting the server
+AUTH_SECRET_TOKEN="${SECRET_TOKEN_NAME}"
+AUTH_SECRET_REFRESH_TOKEN="${SECRET_REFRESH_TOKEN_NAME}"
+
+export AUTH_SECRET_TOKEN
+export AUTH_SECRET_REFRESH_TOKEN
+
+export AUTH_ENDPOINT=localhost
+npm run serve-staging
