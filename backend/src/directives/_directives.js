87175a85e59dc4e05475d6447987590d5fdf6e9c
Rico <e.cerroni@gmail.com>
2017-11-23 12:25:41 +0100

    more descriptive field for function name

--- backend/src/directives/_directives.js
+++ backend/src/directives/_directives.js
@@ -1,15 +1,15 @@
 export const DIRECTIVES = {
   IS_ALLOWED: {
-    NAME: 'isAllowed',
+    FUNC_NAME: 'isAllowed',
     SCOPE: 'scope',
   },
   HAS_ROLE: {
-    NAME: 'hasRole',
+    FUNC_NAME: 'hasRole',
     SCOPE: 'role',
   },
 };
 
 export const directives = `
-  directive @${DIRECTIVES.HAS_ROLE.NAME} (${DIRECTIVES.HAS_ROLE.SCOPE}: [String]) on QUERY | FIELD
-  directive @${DIRECTIVES.IS_ALLOWED.NAME} (${DIRECTIVES.IS_ALLOWED.SCOPE}: [String]) on QUERY | FIELD
+  directive @${DIRECTIVES.HAS_ROLE.FUNC_NAME} (${DIRECTIVES.HAS_ROLE.SCOPE}: [String]) on QUERY | FIELD
+  directive @${DIRECTIVES.IS_ALLOWED.FUNC_NAME} (${DIRECTIVES.IS_ALLOWED.SCOPE}: [String]) on QUERY | FIELD
 `;
--- backend/src/directives/_resolvers.js
+++ backend/src/directives/_resolvers.js
@@ -2,7 +2,7 @@ import { FORBIDDEN } from '@/environment';
 import { DIRECTIVES } from './_directives';
 
 export const directiveResolvers = {
-  [DIRECTIVES.IS_ALLOWED.NAME](result, source, args, context) {
+  [DIRECTIVES.IS_ALLOWED.FUNC_NAME](result, source, args, context) {
     const expectedScopes = args[DIRECTIVES.IS_ALLOWED.SCOPE];
 
     const scopes = context.user
@@ -13,7 +13,7 @@ export const directiveResolvers = {
     // throw Error(FORBIDDEN);
     return null; // if scope is not allowed return null instead of 403
   },
-  [DIRECTIVES.HAS_ROLE.NAME](result, source, args, context) {
+  [DIRECTIVES.HAS_ROLE.FUNC_NAME](result, source, args, context) {
     const expectedRoles = args[DIRECTIVES.HAS_ROLE.SCOPE];
     const roles = context.user
       && context.user.roles;
