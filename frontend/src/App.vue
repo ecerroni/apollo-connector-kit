2462c6f866ff1f8ff7a7dd06d62690a8bbf9ebb2
Rico <e.cerroni@gmail.com>
2017-11-24 18:32:44 +0100

    add initial render transitions + router transitions

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,20 @@
 <template>
-  <div id="app">
-    <top />
-    <img class="logo" src="./assets/logo.png">
-    <router-view></router-view>
-    <bottom />
+  <div id="app" v-cloak>
+    <transition
+      appear
+      appear-class="fade-enter"
+      appear-to-class="fade"
+      appear-active-class="fade-enter-active"
+    >
+      <div class="a">
+        <top />
+        <img class="logo" src="./assets/logo.png">
+        <transition name="slide" mode="out-in">
+          <router-view></router-view>
+        </transition>
+        <bottom />
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -21,6 +32,10 @@ export default {
 html, body {
   margin: 0;
 }
+.a {
+  display: flex;
+  flex-direction: column;
+}
 #app  {
   display: flex;
   height: 100vh;
@@ -32,6 +47,29 @@ html, body {
   text-align: center;
   color: #2c3e50;
 }
+
+[v-cloak] {
+  display: none;
+}
+
+.fade-enter-active {
+  transition: opacity .3s
+}
+.fade-enter {
+  opacity: 0
+}
+
+.fade {
+
+}
+
+.slide-enter { transform: translateX(-100%); }
+.slide-leave-to { opacity: 0; }
+.slide-enter-to { transform: translateX(0) }
+.slide-leave { transform: translateY(0) }
+
+.slide-enter-active { transition: all 350ms ease-in-out }
+.slide-leave-active { transition: all 150ms ease-in-out }
 
 .logo {
   display: flex;
