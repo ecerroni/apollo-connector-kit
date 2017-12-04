4101f90cebd28edcd5644882bbb70678a5d5ea26
Rico <e.cerroni@gmail.com>
2017-12-04 18:36:09 +0100

    createValidateToken allowed only when !== production

--- backend/src/components/Various/general/_mutation.js
+++ backend/src/components/Various/general/_mutation.js
@@ -21,23 +21,25 @@ export const mutationResolvers = {
   ********** DEVELOPMENT ONLY | TO BE USED WITH GRAPHIQL ********
    */
     [PUBLIC_MUTATIONS.CREATE_VALID_TOKEN]: async () => {
-    // CreateValidToken: async () => {
-      const users = await User.all();
-      const user = users[Math.floor(Math.random() * users.length)];
-      const additionalClaims = {};
-      const userData = {
-        id: user.id,
-        username: user.username,
-        email: user.email,
-        roles: user.roles,
-        permissions: user.permissions,
-      };
-      const [token, refreshToken] = await createTokens({
-        user: userData,
-        refreshTokenSecret: user.password + AUTH.SECRET_REFRESH_TOKEN,
-      }, additionalClaims);
-      const response = JSON.stringify({ token, refreshToken });
-      return response;
+      if (process.env.NODE_ENV !== 'production') {
+        const users = await User.all();
+        const user = users[Math.floor(Math.random() * users.length)];
+        const additionalClaims = {};
+        const userData = {
+          id: user.id,
+          username: user.username,
+          email: user.email,
+          roles: user.roles,
+          permissions: user.permissions,
+        };
+        const [token, refreshToken] = await createTokens({
+          user: userData,
+          refreshTokenSecret: user.password + AUTH.SECRET_REFRESH_TOKEN,
+        }, additionalClaims);
+        const response = JSON.stringify({ token, refreshToken });
+        return response;
+      }
+      return {};
     },
   },
 };
