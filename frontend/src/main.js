072982846f4a865cd859e223f57cb593d22e9cbe
Rico <e.cerroni@gmail.com>
2017-12-07 09:49:18 +0100

    don't import the whole elementUI, just single components

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import ElementUI from 'element-ui';
+import { Button, Form, FormItem, Input } from 'element-ui';
 import VueApollo from 'vue-apollo';
-import 'element-ui/lib/theme-chalk/index.css';
+// import 'element-ui/lib/theme-chalk/index.css';
 import App from './App';
 import router from './router';
 import { apolloClient } from './utils';
 
-Vue.use(ElementUI);
+Vue.use(Button);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Input);
 
 Vue.config.productionTip = false;
 
