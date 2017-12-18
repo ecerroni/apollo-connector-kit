7dcd4839177f04267423dc619bdc15911e4bf398
Rico <e.cerroni@gmail.com>
2017-12-18 14:17:24 +0100

    use async routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,8 +2,21 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/components/Home';
 import Login from '@/components/Login';
-import Forbidden from '@/components/Forbidden';
-import NotFound from '@/components/NotFound';
+
+const AsyncForbidden = () => import('@/components/Forbidden');
+
+const AsyncNotFound = () => ({
+  component: import('@/components/NotFound'),
+  // A component to use while the async component is loading
+  // loading: LoadingComp,
+  // A component to use if the load fails
+  // error: ErrorComp,
+  // Delay before showing the loading component. Default: 200ms.
+  // delay: 200,
+  // The error component will be displayed if a timeout is
+  // provided and exceeded. Default: Infinity.
+  // timeout: 3000,
+});
 
 Vue.use(Router);
 
@@ -22,9 +35,9 @@ export default new Router({
     {
       path: '/forbidden',
       name: 'Forbidden',
-      component: Forbidden,
+      component: AsyncForbidden,
     },
-    { path: '/40x', component: NotFound },
+    { path: '/40x', component: AsyncNotFound },
     { path: '*', redirect: '/40x' },
   ],
 });
