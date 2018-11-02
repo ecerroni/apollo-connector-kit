738c7598781f61557ccffc3eb229b85576452f17
ric0 <gemoter@gmaker.me>
2018-11-02 19:42:28 +0100

    Align with the new Apollo 2 req , res pattern

--- backend/src/graphql/_context.js
+++ backend/src/graphql/_context.js
@@ -1,5 +1,5 @@
-export const context = async (req) => {
+export const context = async ({ res, req }) => {
   // Attach additional properties to context if needed
-  return { user: req.user };
+  return { user: req.user, res, req };
 };
 
--- backend/src/graphql/_format-response.js
+++ backend/src/graphql/_format-response.js
@@ -1,35 +1,35 @@
 import { selectAuthStrategy } from '~/authentication';
+import { AUTH } from '~/config'
 import { setCookies, setHeaders, unsetCookies } from '~/authentication';
 
 // TODO: MOVE THEM TO ENUMS OR CONFIG
 const login = ['publicLogin', 'publicRegister'];
 const logout = 'logout';
 
-export const formatResponse = (res, { context }, response, request) => {
-  const operationName = Object.keys(res.data)[0];
-  let extendedResponse = res;
-  if (operationName && extendedResponse.data && operationName === logout) {
-    const [httpOnly] = selectAuthStrategy(request.headers);
+export const formatResponse = ({ response, query}) => {
+  const { context } = query;
+  const { res, req: request } = context;
+  const { data } = response;
+  const { headers = {} } = request;
+  const operationName = data && Object.keys(data)[0];
+  if (operationName && operationName === logout) {
+    const [httpOnly] = selectAuthStrategy(headers);
     if (httpOnly) {
-      unsetCookies(response);
+      unsetCookies(res);
     }
-  }
-  if (operationName && extendedResponse.data && login.includes(operationName)) {
-    const data = JSON.parse(extendedResponse.data[operationName]);
-    if (data) {
-      const [httpOnly, localStorage] = selectAuthStrategy(request.headers);
-      const { token, refreshToken } = data;
+  } 
+  if (data && operationName && login.includes(operationName)) {
+    if (data[operationName]) {
+      const [httpOnly, localStorage] = selectAuthStrategy(headers);
+      const { token, refreshToken } = JSON.parse(data[operationName]);
       if (httpOnly) {
-        setCookies(response, token, refreshToken);
+        setCookies(res, token, refreshToken);
       }
       if (localStorage) {
-        setHeaders(response, token, refreshToken);
+        setHeaders(res, token, refreshToken);
       }
-      extendedResponse = {
-        data: { [operationName]: data },
-      };
     }
   }
-  return extendedResponse;
+  return response;
 };
 
