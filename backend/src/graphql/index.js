7a96ea3e911c1d7a9c404b830058b1c38d38516b
ric0 <gemoter@gmaker.me>
2018-11-02 19:41:05 +0100

    Delete graphiql

--- backend/src/graphql/index.js
+++ backend/src/graphql/index.js
@@ -1,9 +1,6 @@
-import _graphiql from './_graphiql'
-
 export { formatResponse } from './_format-response';
 export { formatError } from './_format-error';
 export { formatParams } from './_format-params';
 export { context } from './_context';
-export { default as graphiql } from './_graphiql';
 export { default as setPublicResolvers } from './_set-public-resolvers';
 export { default as makePublicResolver } from './_make-public-resolver';
