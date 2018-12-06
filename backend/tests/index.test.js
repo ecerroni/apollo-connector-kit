255d69374abae09b56f00ca77138d75968d93f2a
ric0 <e.cerroni@gmail.com>
2018-12-06 20:41:08 +0100

    Add public vs private queries test

--- backend/tests/index.test.js
+++ backend/tests/index.test.js
@@ -2,7 +2,7 @@ import { tester } from 'graphql-tester';
 import decode from 'jwt-decode';
 import { SERVER } from '../src/config';
 // import { to, asyncArray } from '../src/utils';
-import { ERROR } from '../src/environment';
+import { ERROR, UNAUTHORIZED } from '../src/environment';
 
 
 const {
@@ -16,6 +16,18 @@ const loginQuery = `
 mutation login ($userCredentials: userCredentials!) {
   login(input: $userCredentials)
 }`;
+
+const publicTestQuery = `
+  query test {
+    test
+  }
+`;
+
+const privateAuthQuery = `
+  query _checkAuth {
+    _checkAuth
+  }
+`;
 
 // USERS
 describe('A user', function () {
@@ -108,6 +120,46 @@ describe('A user', function () {
       })
       .catch((err) => {
         expect(err).toBe(null);
+        done();
+      });
+  });
+  it('should be allowed to call public queries', (done) => {
+    this
+      .test(
+        JSON.stringify({
+          query: publicTestQuery,
+        }),
+        { jar: true },
+      )
+      .then((res) => {
+        expect(res.status).toBe(200);
+        expect(res.success).toBe(true);
+        const { data: { test = '' } = {} } = res;
+        expect(test).toBe('Server is up and running... working smoothly');
+        done();
+      })
+      .catch(err => {
+        expect(err).toBe(null);
+        done();
+      });
+  });
+  it('should be NOT allowed to call private queries', (done) => {
+    this
+      .test(
+        JSON.stringify({
+          query: privateAuthQuery,
+        }),
+        { jar: true },
+      )
+      .then((res) => {
+        expect(res.status).toBe(200);
+        expect(res.success).toBe(true);
+        const { errors } = res;
+        expect(Array.isArray(errors)).toBe(true);
+        expect(res.errors[0].message).toBe(UNAUTHORIZED);
+        done();
+      })
+      .catch(err => {
         done();
       });
   });
