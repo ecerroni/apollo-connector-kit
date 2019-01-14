d248705a660b0299bdecb149171194d7157cd472
ric0 <e.cerroni@gmail.com>
2019-01-14 17:58:59 +0100

    Implement better handling of env vars for local development and production

---
+++ backend/env/lookenv.dev.js
@@ -0,0 +1,18 @@
+module.exports = {
+  AWS_ACCESS_KEY_ID: {
+    default: '',
+  },
+  AWS_SECRET_ACCESS_KEY: {
+    default: '',
+  },
+  DB_CONNECTION_STING: {
+    default: 'mongodb://localhost:27017/myproject',
+  },
+  AUTH_SECRET_TOKEN: {
+    default: '1234',
+  },
+  AUTH_SECRET_REFRESH_TOKEN: {
+    default: '123456789',
+  },
+  AUTH_ENDPOINT: 'localhost',
+};
