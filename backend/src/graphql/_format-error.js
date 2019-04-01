b83430eeb3950bb46ab8373cd9065ee3a7e533ed
ric0Waat <enrico@waat.eu>
2019-04-01 17:48:51 +0200

    Mask stacktrace errors

--- backend/src/graphql/_format-error.js
+++ backend/src/graphql/_format-error.js
@@ -1,3 +1,6 @@
+import { ApolloError } from 'apollo-server-express';
+import { v4 } from 'uuid';
+import { GraphQLError } from 'graphql';
 import { FORBIDDEN, UNAUTHORIZED, ERROR } from '~/environment';
 
 const e401s = [
@@ -11,13 +14,21 @@ const e403s = [FORBIDDEN];
 // export const formatError = (err, response) => {
 export const formatError = err => { // eslint-disable-line
   let error = err;
+  const maskError = !(error.originalError instanceof ApolloError) && !e401s.includes(err.message) && !e403s.includes(err.message);
+  if (maskError) {
+    const errId = v4();
+    console.log('errId: ', errId);
+    console.log(error);
+
+    return new GraphQLError(`Internal Error: [Log id] ${errId}`);
+  }
   if (e401s.includes(err.message)) {
     // We need this response status in the apollo client afterware
     error = {
       message: err.message,
       status: 401,
       location: err.location,
-      path: err.path,  
+      path: err.path,
       extensions: {
         code: err.extensions.code,
       },
