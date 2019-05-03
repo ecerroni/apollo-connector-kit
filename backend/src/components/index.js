2510f893a36a9251ab283096fc9a4d07bbbdeb52
Enrico <Cerroni>
2019-05-03 14:53:11 +0200

    Fix string

--- backend/src/components/index.js
+++ backend/src/components/index.js
@@ -1,21 +1,35 @@
 import * as all from './**/**/index.js';
 
+const allPaths = Object.entries(all).reduce((arr, entry) => {
+  if (entry && entry[1] && entry[0]) {
+    return [...arr, ...Object.keys(entry[1]).map(key => ({ [key]: `${entry[0]}/${key}` }))];
+  }
+  return [...arr];
+}, []);
+//
 const allComponents = Object.values(all)
   .filter(v => !!v)
-  .map(v => Object.values(v))
-  .reduce((arr, item) =>  [
-    ...arr,
-    ({
-      types: [...item.map(i => i.index.types)],
-      resolvers: [...item.map(i => i.index.resolvers)],
-    })
-  ], []);
+  // .map(v => Object.values(v))
+  .reduce((arr, item) => {
+    const alls = {};
+    Object.keys(item).forEach((i) => {
+      const path = `./${allPaths.filter(p => Object.keys(p)[0] === i)[0][i]}`;
+      alls[i] = require(path);
+    });
+    return [
+      ...arr,
+      ({
+        types: [...Object.keys(item).map(i => alls[i].default.types)],
+        resolvers: [...Object.keys(item).map(i => alls[i].default.resolvers)],
+      }),
+    ];
+  }, []);
 
 export default {
   types: [
-    ...allComponents.reduce((arr, i) => [...arr, ...i.types], [])
+    ...allComponents.reduce((arr, i) => [...arr, ...i.types], []),
   ],
   resolvers: [
-    ...allComponents.reduce((arr, i) => [...arr, ...i.resolvers], [])
+    ...allComponents.reduce((arr, i) => [...arr, ...i.resolvers], []),
   ],
 };
