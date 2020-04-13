41d3a2edd744184fec8455e8e8bf3f98d3782b3f
Enrico Cerroni <e.cerroni@gmail.com>
2020-04-13 20:09:30 +0200

    Implement new roles and permissions approach using groups and single file configuration

--- backend/src/config/_roles-permissions.js
+++ backend/src/config/_roles-permissions.js
@@ -1,32 +1,165 @@
 import SCOPES from './_scopes';
+import ROLES_PERMISSIONS from '../../../settings/roles-permissions.json';
 
-const rolesAndPermissions = {
-  ADMIN: {
-    SPEC: SCOPES.ROLES.ADMIN,
-    PERMISSIONS: {
-      [SCOPES.OPERATION.READ]: [
-        SCOPES.TYPE.PROFILE
-        // add more
-      ],
-      [SCOPES.OPERATION.CREATE]: [
-        SCOPES.TYPE.PROFILE
-        // add more
-      ]
-    }
-  },
-  USER: {
-    SPEC: SCOPES.ROLES.USER,
-    PERMISSIONS: {
-      [SCOPES.OPERATION.READ]: [
-        // add more
-      ],
-      [SCOPES.OPERATION.CREATE]: [
-        // add more
-      ]
-    }
-  }
-  // add more
+const { OPERATION, USERS: RAWUSERS, GROUPS } = ROLES_PERMISSIONS;
+
+const validateScopesAndPermissions = permissions => {
+  // SCOPES AND OPERATIONS IN PERMISSIONS SHOULD ALWAYS BE VALID
+  Object.entries(permissions).forEach(function([scope, operations]) {
+    if (!SCOPES.TYPE[scope])
+      throw new Error(
+        `${scope} not included in [${Object.keys(SCOPES.TYPE).join(',')}]`
+      );
+    operations.forEach(function(operation) {
+      if (!OPERATION[operation])
+        throw new Error(
+          `${operation} not included in [${Object.keys(SCOPES.OPERATION).join(
+            ','
+          )}]`
+        );
+    });
+  });
 };
+
+const validateUserGroups = (groups, user) => {
+  groups.forEach(function(group) {
+    if (!Object.keys(GROUPS).includes(group))
+      throw new Error(
+        `${group} of ${user} must be one of [${Object.keys(GROUPS).join(',')}]`
+      );
+  });
+};
+
+const buildPermissions = permissions => {
+  return Object.entries(permissions).reduce(
+    (obj, [key, value]) => ({
+      ...obj,
+      ...value.reduce(
+        (o, v) => ({
+          ...o,
+          [OPERATION[v]]: obj[OPERATION[v]]
+            ? [...obj[OPERATION[v]], SCOPES.TYPE[key]]
+            : [SCOPES.TYPE[key]]
+        }),
+        {}
+      )
+    }),
+    {}
+  );
+};
+
+const USERS = RAWUSERS.reduce((obj, user) => {
+  // do not want duplicates
+  // user can appeary only once in the json hirearchy
+  const keys = Array.isArray(user)
+    ? user.map(u => Object.keys(u)[0])
+    : [Object.keys(user)[0]];
+
+  if (keys.findIndex(key => obj[key]) > -1)
+    throw new Error('Duplicate user type');
+  return {
+    ...obj,
+    ...keys.reduce(
+      (o, key) => {
+        // value can be either an object or an array
+        const value = RAWUSERS.find(item => {
+          if (Array.isArray(item)) {
+            return item.findIndex(i => Object.keys(i)[0] === key) > -1;
+          }
+          return Object.keys(item)[0] === key;
+        });
+        return {
+          ...o,
+          [key]: Array.isArray(value)
+            ? value.find(v => Object.keys(v)[0] === key)[key]
+            : value[key]
+        };
+      },
+      {
+        OWNER: { PERMISSIONS: {} }
+      }
+    )
+  };
+}, {});
+
+let ownerPermissions = {};
+
+const rolesAndPermissions = Object.entries(SCOPES.ROLES).reduce(
+  (obj, [key]) => {
+    validateScopesAndPermissions(USERS[key].PERMISSIONS);
+    const hasGroup =
+      USERS[key].GROUPS &&
+      Array.isArray(USERS[key].GROUPS) &&
+      USERS[key].GROUPS.length;
+    let permissions = buildPermissions(USERS[key].PERMISSIONS);
+
+    if (hasGroup) {
+      // ADD GROUP PERMISSIONS TO THIS USER
+      const userGroups = USERS[key].GROUPS;
+      validateUserGroups(userGroups, key);
+      const userPermissions = { ...permissions };
+      const groupPermissions = {
+        // eslint-disable-next-line no-shadow
+        ...userGroups.reduce((obj, group) => {
+          // eslint-disable-next-line no-shadow
+          const groupPermissions = GROUPS[group].PERMISSIONS;
+          validateScopesAndPermissions(groupPermissions);
+          return {
+            ...obj,
+            ...buildPermissions(groupPermissions)
+          };
+        }, {})
+      };
+      permissions = {
+        ...Object.entries(groupPermissions).reduce(
+          // eslint-disable-next-line no-shadow
+          (obj, [key, permissions]) => {
+            if (userPermissions[key])
+              return {
+                ...obj,
+                [key]: Array.from(
+                  new Set([...userPermissions[key], ...permissions])
+                )
+              };
+            return {
+              ...obj,
+              [key]: permissions
+            };
+          },
+          {}
+        )
+      };
+    }
+    // FEED OWNER PERMISSIONS - IT SHOULD HAVE THEM ALL
+    ownerPermissions = {
+      // eslint-disable-next-line no-shadow
+      ...Object.entries(permissions).reduce((obj, [key, value]) => {
+        console.log(key, value);
+        if (ownerPermissions[key])
+          return {
+            ...ownerPermissions,
+            ...obj,
+            [key]: Array.from(new Set([...ownerPermissions[key], ...value]))
+          };
+        return {
+          ...ownerPermissions,
+          ...obj,
+          [key]: value
+        };
+      }, {})
+    };
+    return {
+      ...obj,
+      [key]: {
+        SPEC: SCOPES.ROLES[key],
+        PERMISSIONS: permissions
+      }
+    };
+  },
+  {}
+);
+
+rolesAndPermissions.OWNER.PERMISSIONS = ownerPermissions;
 console.log('####################################################');
 console.log('ROLES AND PERMISSIONS BINDING');
 console.log('####################################################');
--- backend/src/config/_scopes.js
+++ backend/src/config/_scopes.js
@@ -1,29 +1,43 @@
-export default {
-  OPERATION: {
-    READ: 'read',
-    EDIT: 'edit',
-    CREATE: 'create',
-    DELETE: 'delete'
-    // add more
-  },
-  TYPE: {
-    PROFILE: 'profile',
-    BILLING: 'profile'
-    // add more
-  },
-  ROLES: {
-    OWNER: {
-      RANK: 0,
-      VALUE: 'OWNER'
-    },
-    ADMIN: {
-      RANK: 1,
-      VALUE: 'ADMIN'
-    },
-    USER: {
-      RANK: 2,
-      VALUE: 'USER'
+import ROLES_PERMISSIONS from '../../../settings/roles-permissions.json';
+
+const { OPERATION, USERS, SCOPES } = ROLES_PERMISSIONS;
+
+const OWNER = {
+  RANK: 0,
+  VALUE: 'OWNER'
+};
+
+const ROLES = USERS.reduce(
+  (obj, type, index) => {
+    let values = {};
+    if (Array.isArray(type)) {
+      values = type.reduce(
+        (o, item) => ({
+          ...o,
+          [Object.keys(item)[0]]: {
+            RANK: index + 1,
+            VALUE: Object.keys(item)[0]
+          }
+        }),
+        {}
+      );
+    } else {
+      values = {
+        [Object.keys(type)[0]]: {
+          RANK: index + 1,
+          VALUE: Object.keys(type)[0]
+        }
+      };
     }
-    // add more
-  }
+    return {
+      ...obj,
+      ...values
+    };
+  },
+  { OWNER }
+);
+export default {
+  OPERATION,
+  TYPE: SCOPES,
+  ROLES
 };
